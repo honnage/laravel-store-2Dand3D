b2295e0f54a3f018301888d7ff8e5f50481890ea
unknown <hon_da-1998@hotmail.com>
2021-08-12 02:27:07 +0700

    Vaildate User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('isStatus')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('image')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
